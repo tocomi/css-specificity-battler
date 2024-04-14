8267973e4689afd54f9431854f9b02cc51d646d8
tocomi <tocomi0112@gmail.com>
2024-04-14 20:26:31 +0900

    feat: window size

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,10 +1,10 @@
 import { MainScreen } from '../app/features/battle';
 
 const Popup = () => {
-  document.body.className = 'w-[50rem] h-[30rem]';
+  document.body.className = 'w-[50rem] h-[36rem]';
 
   return (
-    <div className="p-2 h-[30rem]">
+    <div className="p-2 h-[36rem]">
       <MainScreen />
     </div>
   );
